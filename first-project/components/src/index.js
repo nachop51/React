5e0ba6e85832cc5a59c734e547536f1c8f5d912a
Nacho Peralta <nachoperalta0@gmail.com>
2022-09-20 01:02:56 -0300

    advances

--- first-project/components/src/index.js
+++ first-project/components/src/index.js
@@ -1,6 +1,10 @@
+//jshint esversion:6
 import React from "react";
 import ReactDOM from "react-dom/client";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import "./style/App.css";
+import { faker } from "@faker-js/faker";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
@@ -8,11 +12,30 @@ const root = ReactDOM.createRoot(element);
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          image={faker.image.image()}
+          text="Nice blog post!"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00PM"
+          image={faker.image.image()}
+          text="I like the subject"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          image={faker.image.image()}
+          text="I like the writing"
+        />
+      </ApprovalCard>
     </div>
   );
 };
