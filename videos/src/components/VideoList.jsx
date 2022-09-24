25b3c8dcadd1b8c9621e78b07cfe7fee85569ff8
Nacho Peralta <nachoperalta0@gmail.com>
2022-09-24 15:43:22 -0300

    video searching project

--- videos/src/components/VideoList.jsx
+++ videos/src/components/VideoList.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const video_list = videos.map((video) => {
-    return <VideoItem video={video} />;
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
   });
 
   return <div className="ui relaxed divided list">{video_list}</div>;
