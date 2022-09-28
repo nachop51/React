5909e5aae03246e8ff1163f53ea2cf687dff4c9f
Nachop51 <nachoperalta0@gmail.com>
2022-09-27 23:10:31 -0300

    removing some html

--- widgets/src/components/App.jsx
+++ widgets/src/components/App.jsx
@@ -1,27 +1,44 @@
 import React from "react";
-import Accordion from "./Accordion";
-import Search from "./Search";
+// import Accordion from "./Accordion";
+// import Search from "./Search";
+import Dropdown from "./Dropdown";
 
-const items = [
+// const items = [
+//   {
+//     title: "What is React?",
+//     content: "React is a front end javascript framework",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating components",
+//   },
+// ];
+
+const options = [
   {
-    title: "What is React?",
-    content: "React is a front end javascript framework",
+    label: "The Color Red",
+    value: "red",
   },
   {
-    title: "Why use React?",
-    content: "React is a favorite JS library among engineers",
+    label: "The Color Green",
+    value: "green",
   },
   {
-    title: "How do you use React?",
-    content: "You use React by creating components",
+    label: "A Shade of Blue",
+    value: "blue",
   },
 ];
 
 const App = () => {
   return (
     <React.Fragment>
-      <Accordion items={items} />
-      <Search />
+      {/* <Accordion items={items} />
+       <Search /> */}
+      <Dropdown options={options} />
     </React.Fragment>
   );
 };
