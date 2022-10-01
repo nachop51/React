9c14aca48a47faf72cd6d75028948d0bd5eb499f
Nacho Peralta <nachoperalta0@gmail.com>
2022-09-30 21:23:36 -0300

    redux implemented

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import reducers from "./reducers";
 import App from "./components/App";
 
 const element = document.getElementById("root");
 const root = ReactDOM.createRoot(element);
 
-root.render(<App />);
+root.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>
+);
