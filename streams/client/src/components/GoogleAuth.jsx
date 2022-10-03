24a29b26a14c5dab9ee440f946f546865943fa8b
Nacho Peralta <nachoperalta0@gmail.com>
2022-10-03 16:30:35 -0300

    google auth

---
+++ streams/client/src/components/GoogleAuth.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+
+class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId: "",
+          scope: "email",
+          plugin_name: "streamy",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  renderAuthButton() {
+    console.log(this.state.isSignedIn);
+    if (this.state.isSignedIn === null) {
+      return null;
+    } else if (this.isSignedIn) {
+      return (
+        <button className="ui red google button" onClick={this.onSignOut}>
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui red google button" onClick={this.onSignIn}>
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
+    }
+  }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
