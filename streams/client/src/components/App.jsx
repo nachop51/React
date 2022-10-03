bc11852d97f8fd668f68ee185169ef407d1299f9
Nacho Peralta <nachoperalta0@gmail.com>
2022-10-03 00:28:35 -0300

    blog project finished, new streams project

---
+++ streams/client/src/components/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ streams/client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const element = document.getElementById("root");
+const root = ReactDOM.createRoot(element);
+
+root.render(<App />);
